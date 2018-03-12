8ea65e1b606d10edc38b76175a3eecbf4655bca2
Kartik Arora <33497301+ktech99@users.noreply.github.com>
2018-03-12 00:09:51 -0700

    Fixed variable name errors

--- Wallet.java
+++ Wallet.java
@@ -1,16 +1,20 @@
 // The following code is from
 // https://gist.github.com/CryptoKass/6f540a2cd67ebad81a057ea3a50e7f09#file-wallet-java
+// This class stores the public and private key pair that will be used for transactions
 
 import java.security.*;
 import java.security.spec.*;
+import java.util.*;
 
 public class Wallet {
   public PrivateKey privateKey;
   public PublicKey publicKey;
+  public Map<String, TransactionOutput> UTXOs;
 
   public Wallet()
       throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchProviderException {
     generateKeyPair();
+    UTXOs = new HashMap<String, TransactionOutput>();
   }
 
   // Generating the keypairs using Elliptical curve Cryptography
@@ -23,5 +27,41 @@ public class Wallet {
     KeyPair keyPair = keyGen.generateKeyPair();
     privateKey = keyPair.getPrivate();
     publicKey = keyPair.getPublic();
+  }
+
+  public float getBalance() {
+    float total = 0;
+    for (Map.Entry<String, TransactionOutput> item : BlockChain.UTXOs.entrySet()) {
+      TransactionOutput UTXO = item.getValue();
+      if (UTXO.isMine(publicKey)) {
+        UTXOs.put(UTXO.id, UTXO);
+        total += UTXO.value;
+      }
+    }
+    return total;
+  }
+
+  public Transaction sendFunds(PublicKey recieverKey, float value) {
+    if (getBalance() < value) {
+      System.out.println("Not enough funds. You have :" + getBalance());
+      return null;
+    }
+    ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+    float total = 0;
+    for (Map.Entry<String, TransactionOutput> item : UTXOs.entrySet()) {
+      TransactionOutput UTXO = item.getValue();
+      total += UTXO.value;
+      inputs.add(new TransactionInput(UTXO.id));
+      if (total > value) {
+        break;
+      }
+    }
+
+    Transaction newTransaction = new Transaction(publicKey, recieverKey, value, inputs);
+    newTransaction.generateSignature(privateKey);
+    for (TransactionInput input : inputs) {
+      UTXOs.remove(input.transactionOutputId);
+    }
+    return newTransaction;
   }
 }
